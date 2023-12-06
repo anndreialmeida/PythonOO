8f0a3bf18c3febd5e25c0d875615ca8a3f2da846
anndreialmeida <anndrei.almeida@gmail.com>
2023-12-06 04:07:24 -0300

    implmentando a classe filme

--- principal.py
+++ principal.py
@@ -1,4 +1,5 @@
 from conta import Conta
+from filme import Filme
 
 conta = Conta(titular="Andrei", saldo=50, limite=1500)
 conta2 = Conta(titular="Saci", saldo=5, limite=10)
@@ -16,12 +17,13 @@ conta.transfere(50, conta2)
 conta.extrato()
 conta2.extrato()
 
-print(Conta.codigo_banco())
-
-print(Conta.codigo_bancos())
-
-lista_bancos = Conta.codigo_bancos()
-
-print(lista_bancos['BB'])
 
 
+
+filme = Filme(nome="A volta dos que não foram", ano=1992, duracao=150 )
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+print(vingadores.nome)
+
+atlanta = Serie('atlanta', 2018, 2)
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
